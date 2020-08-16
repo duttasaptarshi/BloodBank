4999ed6021fd0459ea3ef7944bceae5dab32c7a6
duttasaptarshi <68784363+duttasaptarshi@users.noreply.github.com>
2020-08-16 16:59:16 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/blogs','BlogController@store');
+
+Route::get('/donate', 'DonateController@create');
+Route::post('/donate', 'DonateController@store');
+
+Route::get('/requests', 'RequestsController@create');
+Route::post('/requests', 'RequestsController@store');
+
+Route::get('/submited', function(){
+    return view('submited');
+});
+
+Route::get('/stocks', function(){
+    return view('stocks',[
+        'stocks' => App\Stocks::latest()->get()
+    ]);
+});
+
+Route::get('/profiles/{user}','ProfilesController@show')->name('profile');
+Route::get('/logout','\App\Http\Controllers\Auth\LoginController@logout');
+
+
+
+Route::get('/aboutus', function(){
+    return view('aboutus');
+});
+
+Route::get('/contactus', function(){
+    return view('contactus');
+});
+
+Auth::routes();
+
+
+
+
+
